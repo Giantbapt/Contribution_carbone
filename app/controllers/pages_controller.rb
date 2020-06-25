59f729e646c217f7a908efa8bf5fb63c071cb9fd
Baptiste Chebassier <baptiste.chebassier28@gmail.com>
2020-06-25 16:40:54 +0200

    com

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def resultat
+  end
+
+  def ressources
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  
+  get 'resultat', to: 'pages#resultat', as: :resultat
+  get 'ressources', to: 'pages#ressources', as: :ressources
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
