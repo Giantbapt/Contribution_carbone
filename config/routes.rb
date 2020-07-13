f457246265db32a97908ff8ca6c524817a21820f
Baptiste Chebassier <baptiste.chebassier28@gmail.com>
2020-07-14 00:03:29 +0200

    shares buttons

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   get 'ressources', to: 'pages#ressources', as: :ressources
   get 'questionnaire', to: 'pages#questionnaire', as: :questionnaire
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+   get '/404', to: "errors#not_found"
+    get '/422', to: "errors#unacceptable"
+    get '/500', to: "errors#internal_error"
 end
