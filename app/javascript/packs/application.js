ea51597c81b9e35e026b5a466a842f34f6046c52
Baptiste Chebassier <baptiste.chebassier28@gmail.com>
2020-09-16 17:54:10 +0200

    add progressive bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,3 +32,7 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+window.addEventListener("hashchange", function () {
+    window.scrollTo(window.scrollX, window.scrollY - 100);
+});
